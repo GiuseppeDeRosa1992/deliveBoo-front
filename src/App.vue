f5fec5649be6e261244fb507aed145ffb320a6dc
sidane17 <khalilsidane89@gmail.com>
2024-09-03 17:25:54 +0200

    ricerca funzionante

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,60 @@ import axios from 'axios';
 
 export default{
   name : 'App',
+
   data(){
     return {
       restaurants:[],
+      error: null,
+      ricercaRistorante:'',
+    }
+  },
+ mounted(){
+    this.chiamataRestaurant();
+  
+},
+
+  methods:{
+    chiamataRestaurant() {
+      axios.get('http://127.0.0.1:8000/api/restaurant')
+        .then(result => {
+          this.restaurants = result.data;
+        })
+        .catch(error => {
+          console.error('Errore nel recupero dei dati:', error);
+          this.error = 'Errore nel caricamento dei ristoranti'; 
+        });
     }
   },
 
-  mounted(){
-    axios.get('http://127.0.0.1:8000/api/restaurant')
-    .then(result=>{
-      this.restaurants=result.data;
-    })
+  computed: {
+    filteredRestaurants() {
+      return this.restaurants.filter(restaurant => {
+        return restaurant.name.toLowerCase().includes(this.ricercaRistorante.toLowerCase());
+      });
+    }
   }
-  
 }
+
+ 
 </script>
 
 <template>
-  <h1>Ristoranti </h1>
-  <ul>
-      <li v-for="restaurant in restaurants">
+  <div>
+    <h1>Ristoranti</h1>
+
+    <input type="text" v-model="ricercaRistorante" placeholder="Cerca un ristorante..." />
+
+    <p v-if="filteredRestaurants.length === 0">Nessun ristorante trovato</p>
+
+    <ul v-if="filteredRestaurants.length > 0">
+      <li v-for="restaurant in filteredRestaurants" :key="restaurant.id">
         {{ restaurant.name }}
       </li>
     </ul>
 
+    <p v-if="error">{{ error }}</p>
+  </div>
 </template>
 
 <style scoped>
