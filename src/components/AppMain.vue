3185a857aafe2c217556faf75466d15942706e77
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-09-04 16:48:26 +0200

    added header and little style to main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,7 +11,6 @@ export default{
       error: null,
       ricercaRistorante:'',
       base_url: 'http://127.0.0.1:8000',
-      filtroType: [],
       tipoSelezionato: null
     }
   },
@@ -61,55 +60,52 @@ export default{
 </script>
 
 <template>
-  <div>
-    
-    <h1>Ristoranti</h1>
+  <div class=" text-center light-orange">
+    <div class="container ">
+      <div class="flex row justify-content-center align-items-center">
+      <h1 class="">Benvenuto su Deliveboo</h1>
+      <h3>Il sapore che ti raggiunge</h3>
+        
+        <!-- Input per la ricerca del ristorante -->
+        <div class="my-3 w-25">
+          <input type="text" class="form-control" v-model="ricercaRistorante" id="ricercaRistorante" placeholder="Ricerca un ristorante specifico">
+        </div>
 
-    <input class="search" type="text" v-model="ricercaRistorante" placeholder="Cerca un ristorante..." />
-
-    <div class="filter-section">
-        <select class="form-select" @change="filtroType($event.target.value)"> 
-            <option selected>Scegli cosa vuoi mangiare</option>
-            <option v-for="type in types" :key="type.name" :value="type.name"><{{type.name}}</option>
-        </select>
-    </div>
-
-    <p v-if="filteredRestaurants.length === 0">Nessun ristorante trovato</p>
-    <div class="container flex row">
-    <template v-if="filteredRestaurants.length > 0">
-      <div class="card col-2 p-2 m-3" v-for="restaurant in filteredRestaurants">
-        <template v-if="!restaurant.image.startsWith('http')">
-          <img class="card-img-top" :src="base_url + '/storage/' + restaurant.image" alt="">
-        </template>
-        <template v-else>
-          <img class="card-img-top" :src="restaurant.image" alt="">
-        </template>
-        <h2 class="title-card">{{ restaurant.name }}</h2>
-        <p class="info-card">Indirizzo: {{ restaurant.address }}</p>
+        <!-- Select attuale per filtrare Categoria del Ristorante -->
+        <div class="filter-section w-25">
+            <select class="form-select" @change="filtroType($event.target.value)"> 
+              <option selected>Scegli cosa vuoi mangiare</option>
+              <option v-for="type in types" :key="type.name" :value="type.name"><{{type.name}}</option>
+            </select>
+        </div>
       </div>
-    </template>
 
-  </div>
+      <p class="my-3" v-if="filteredRestaurants.length === 0">Nessun ristorante trovato</p>
+    </div>
+    
+    <!-- Card Container con cards -->
+    <div class="container">
+      <div class="flex row my-3">
+        <template v-if="filteredRestaurants.length > 0">
+          <div class="card col-2 p-2 m-3" v-for="restaurant in filteredRestaurants">
+          <template v-if="!restaurant.image.startsWith('http')">
+            <img class="card-img-top" :src="base_url + '/storage/' + restaurant.image" alt="">
+          </template>
+          <template v-else>
+            <img class="card-img-top" :src="restaurant.image" alt="">
+          </template>
+            <h2 class="title-card">{{ restaurant.name }}</h2>
+            <p class="card-text">Indirizzo: {{ restaurant.address }}</p>
+          </div>
+        </template>
+      </div>
+    </div>
     <p v-if="error">{{ error }}</p>
   </div>
 </template>
 
 <style scoped>
-.cont-flex {
-  display: flex;
-  width: 1200px;
-  flex-wrap: wrap;
-  gap: 20px;
-}
 
-.search {
-  margin: 20px 0 50px 0;
-  width: 250px;
-}
-
-.info-card {
-  margin: 10px 0 0 0;
-}
 </style>
 
 
