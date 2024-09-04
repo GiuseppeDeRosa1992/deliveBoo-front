e81844d5a24a53527a28bc411e74675d35257342
francesco madonna <160396542+francescomadonna712@users.noreply.github.com>
2024-09-04 16:30:58 +0200

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
-<script >
+<script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from "./components/AppMain.vue";
 import AppFooter from "./components/AppFooter.vue";
 
-export default{
-  name : 'App',
+export default {
+  name: 'App',
 
   components: {
     AppHeader,
@@ -14,17 +14,18 @@ export default{
 
 }
 
- 
+
 </script>
 
 <template>
-    <AppHeader />
-    
-    <AppMain />
 
-    <AppFooter />
+  <AppHeader />
+
+  <AppMain />
+
+  <AppFooter />
+
+
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
